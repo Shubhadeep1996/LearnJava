e49cc60295bf7688a4c6974a0a1daf6078ac722b
Shubhadeep Naskar <43546012+Shubhadeep1996@users.noreply.github.com>
2025-01-22 10:34:12 +0530

    Renamed the Package FirstJava>HelloWorld

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,6 +1,6 @@
-package FirstJava;
+package HelloWorld;
 
-public class FirstJava {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Java!");
     }
